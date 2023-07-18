51e78ac3f5591ce6464c8229bc502346b80706c2
emmaahong <emmahong06@gmail.com>
2023-07-18 09:45:29 -0400

    small edits here and there new files, html files and basic structure + db

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, request, flash, redirect, jsonify, session
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # SQLite URI compatible - checking if OS is Windows or Linux/MacOS to make sure prefix is right
 WIN = sys.platform.startswith('win')
@@ -15,11 +16,11 @@ else:
 
 # app and database path initialization [25]
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ics4u'
+app.config['SECRET_KEY'] = 'emma'
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'database.db')
+print(app.config['SQLALCHEMY_DATABASE_URI'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# initializing database and login manager
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
@@ -33,7 +34,5 @@ def about():
 
 if __name__ == "__main__":
     with app.app_context():
-        # creating all databases that were initialized and running web app
         db.create_all()
-        
     app.run(debug=True, port=5000)
